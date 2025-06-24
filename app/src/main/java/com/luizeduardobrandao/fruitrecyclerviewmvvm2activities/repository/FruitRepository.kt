9cc37a8241936ca303d84d18d7b47a96f18a7ace
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-24 08:17:33 -0300

    implementando logica para ler base de dados json e converter em list

--- app/src/main/java/com/luizeduardobrandao/fruitrecyclerviewmvvm2activities/repository/FruitRepository.kt
+++ app/src/main/java/com/luizeduardobrandao/fruitrecyclerviewmvvm2activities/repository/FruitRepository.kt
@@ -1,4 +1,21 @@
 package com.luizeduardobrandao.fruitrecyclerviewmvvm2activities.repository
 
-class FruitRepository {
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.luizeduardobrandao.fruitrecyclerviewmvvm2activities.R
+import com.luizeduardobrandao.fruitrecyclerviewmvvm2activities.entity.FruitEntity
+
+class FruitRepository(private val context: Context) {
+
+    fun getFruits(): List<FruitEntity> {
+
+        // abre o raw resouce
+        val input = context.resources.openRawResource(R.raw.fruits)
+        val json = input.bufferedReader().use { it.readText() }
+
+        // converte JSON em List<FruitEntity> usando Gson
+        val listType = object: TypeToken<List<FruitEntity>>() {}.type
+        return Gson().fromJson(json, listType)
+    }
 }
